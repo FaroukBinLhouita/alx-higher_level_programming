b115a3856c586caf52098d6ddb2009f23148208d
farouk <faroukhoty@gmail.com>
2023-11-27 21:03:47 +0200

    all files done

---
+++ 10-check_cycle.c
@@ -0,0 +1,34 @@
+#include "list.h"
+
+/**
+ * check_cycle - check if there is a cycle or not
+ * 
+ * 
+ */
+int check_cycle(listint_t *list)
+{
+    listint_t *now, *next;
+    if (list == NULL)
+    {
+        return (0);
+    }
+
+    now = list;
+    next = list;
+
+    while (now && next && next->next)
+    {
+        now = now->next;
+        next = next->next
+        if (now == next)
+        {
+            return (1)
+        }
+    }
+    
+    if (now != next)
+    {
+        return (0);
+    }
+    return (1);
+}
